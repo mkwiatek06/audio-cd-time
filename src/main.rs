b1692e5c2afc9790e384d326d3f754eb41189534
Mateusz Kwiatek <mk322108@student.polsl.pl>
2026-05-16 17:12:41 +0200

    use current directory when called without args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use colored::Colorize;
 use lofty::{file::AudioFile, probe::Probe};
-use std::env;
+use std::{env, path::PathBuf};
 use walkdir::WalkDir;
 
 macro_rules! minutes {
@@ -15,7 +15,7 @@ macro_rules! seconds {
     };
 }
 
-fn display(pre: &str, time_ns: &u128) {
+fn display(pre: String, time_ns: &u128) {
     if *time_ns > 4_800_000_000_000 {
         // > 80 minutes
         println!(
@@ -40,20 +40,13 @@ fn display(pre: &str, time_ns: &u128) {
     }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+trait Compute {
+    fn compute(self, _: &mut u128);
+}
 
-    let mut multi_folder = false;
-    if args.len() > 2 {
-        multi_folder = true;
-    }
-
-    let mut duration_comb_i: u128 = 0;
-
-    for folder in args.iter().skip(1) {
-        let mut duration_comb_f: u128 = 0;
-
-        for entry in WalkDir::new(folder) {
+impl Compute for &String {
+    fn compute(self, combined_duration: &mut u128) {
+        for entry in WalkDir::new(self) {
             let entry = match entry {
                 Ok(v) => v,
                 Err(_) => continue,
@@ -67,26 +60,81 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Err(_) => continue,
             };
 
-            let duration = file.properties().duration().as_nanos();
-            duration_comb_f += duration;
+            *combined_duration += file.properties().duration().as_nanos();
         }
+    }
+}
 
-        let canonical = std::fs::canonicalize(folder)?;
+impl Compute for &PathBuf {
+    fn compute(self, combined_duration: &mut u128) {
+        for entry in WalkDir::new(self) {
+            let entry = match entry {
+                Ok(v) => v,
+                Err(_) => continue,
+            };
+
+            let file = match Probe::open(entry.path()) {
+                Ok(probe) => match probe.read() {
+                    Ok(v) => v,
+                    Err(_) => continue,
+                },
+                Err(_) => continue,
+            };
+
+            *combined_duration += file.properties().duration().as_nanos();
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        let current_dir = match env::current_dir() {
+            Ok(v) => v,
+            Err(e) => panic!("Failed to get current directory: {e}"),
+        };
+        let mut duration_comb_f: u128 = 0;
+
+        current_dir.compute(&mut duration_comb_f);
+
+        let folder_name = current_dir
+            .file_name()
+            .and_then(|name| name.to_str())
+            .unwrap_or("unknown");
+
+        display(format!("::: {}:", folder_name), &duration_comb_f);
+
+        return Ok(());
+    }
+
+    let mut duration_comb_i: u128 = 0;
+    let mut multi_arg = false;
+    if args.len() > 2 {
+        multi_arg = true;
+    }
+
+    for arg in args.iter().skip(1) {
+        let mut duration_comb_f: u128 = 0;
+
+        arg.compute(&mut duration_comb_f);
+
+        let canonical = std::fs::canonicalize(arg)?;
         let folder_name = canonical
             .file_name()
             .and_then(|name| name.to_str())
             .unwrap_or("unknown");
 
-        display(&format!("::: {}:", folder_name), &duration_comb_f);
+        display(format!("::: {}:", folder_name), &duration_comb_f);
 
-        if multi_folder {
+        if multi_arg {
             duration_comb_i += duration_comb_f;
         }
     }
 
-    if multi_folder {
+    if multi_arg {
         println!("");
-        display(":+: COMBINED:", &duration_comb_i)
+        display(":+: COMBINED:".to_string(), &duration_comb_i)
     }
 
     Ok(())
